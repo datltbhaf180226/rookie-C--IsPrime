dfa899167e6eff336b3492ab79977d6294acef34
datltbhaf180226 <73823326+datltbhaf180226@users.noreply.github.com>
2021-03-23 23:55:40 +0700

    IsPrime

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;  
+            int i, j;
+            for (i = 2; i <= 100; i++) {  
+                for (j = 2; j <= 100; j++) {  
+                    if (i != j && i % j == 0) {  
+                        isPrime = false;  
+                        break;  
+                    }  
+                }  
+                if (isPrime) {  
+                    Console.WriteLine($"{i} is a prime number");  
+                }  
+                isPrime = true;  
+            }
+        }
+    }
+}
